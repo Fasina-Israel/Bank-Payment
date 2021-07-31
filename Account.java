7288a1c3391c69a8929fef39f7d634bb3d706eac
Fasina Israel <Fashman28@gmail.com>
2021-07-31 05:17:50 +0100

    Hello world

---
+++ Account.java
@@ -0,0 +1,21 @@
+public class Account{
+   private  String name;
+   private double balance;
+}
+  
+ public void setAccountName(String name){
+     this.name = name;
+}
+ public String getAccountName(){
+   return name;
+}
+ public void setAccountBalance(double balance){
+    this.balance = balance;
+}
+public double getAccountBalance(){
+   return balance;
+}
+  
+  
+
+}
